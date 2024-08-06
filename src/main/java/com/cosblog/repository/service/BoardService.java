6a0a02383a68c87aa859b25eb56c40934632ae84
jdh94 <pnewlife@naver.com>
2024-08-06 23:48:58 +0900

    메소드명 변경 작업

--- src/main/java/com/cosblog/repository/service/BoardService.java
+++ src/main/java/com/cosblog/repository/service/BoardService.java
@@ -24,31 +24,31 @@ import ch.qos.logback.core.encoder.Encoder;
 //스프링이 컴포넌트 스캔을 통해서  Bean에 등록해줌. IoC를 해준다.
 @Service
 public class BoardService {
-	
+
 	@Autowired
 	private BoardRepository boardRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder encoder;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private ReplyRepository replyRepository;
-	
+
 	@Transactional
 	public void 글쓰기(Board board, User user) { // title, content
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
-	} 
-	
+	}
+
 	@Transactional(readOnly = true)
-	public Page<Board> 글목록(Pageable pageable){
+	public Page<Board> boradList(Pageable pageable){
 		return boardRepository.findAll(pageable);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Board 글상세보기(int id) {
 		return boardRepository.findById(id)
@@ -56,12 +56,12 @@ public class BoardService {
 				return new IllegalArgumentException("글 상세보기 실패:아이디를 찾을 수 없습니다.");
 			});
 	}
-	
+
 	@Transactional
 	public void 글삭제하기(int id) {
 		boardRepository.deleteById(id);
 	}
-	
+
 	@Transactional
 	public void 글수정하기(int id, Board requestBoard) {
 		Board board = boardRepository.findById(id)
@@ -72,15 +72,15 @@ public class BoardService {
 		board.setContent(requestBoard.getContent());
 		//해당 함수로 종료수(Service가 종료될 때) 트랜잭션이 종료됩니다. 이때 더티체킹 - 자동업데이트가 flush
 	}
-	
+
 	@Transactional
 	public void 댓글쓰기(ReplySaveRequestDto replySaveRequestDto) {
 
-		
-		
+
+
 		replyRepository.mSave(replySaveRequestDto.getUserId(), replySaveRequestDto.getBoardId(), replySaveRequestDto.getContent());
 	}
-	
+
 	@Transactional
 	public void 댓글삭제(int replyId) {
 		replyRepository.deleteById(replyId);
